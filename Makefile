e630853ae5c70706220c2a39e03fe7e0a22d849f
Richard Dymond <rjdymond@gmail.com>
2013-03-24 12:04:59 -0300

    Add get-images.py

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ usage:
 	@echo "Supported targets:"
 	@echo "  usage    show this help"
 	@echo "  scripts  create the game launcher scripts"
+	@echo "  images   create the stock Pyskool images in ~/.pyskool"
 	@echo "  doc      build the documentation"
 	@echo "  clean    clean the documentation"
 
@@ -11,6 +12,10 @@ scripts:
 	@for s in skool_daze.py back_to_skool.py skool_daze_take_too.py ezad_looks.py back_to_skool_daze.py; do \
 	    ln -sf pyskool.py $$s; \
 	done
+
+.PHONY: images
+images:
+	PYSKOOL_HOME=. utils/get-images.py ~/.pyskool
 
 .PHONY: doc
 doc:
---
+++ utils/get-images.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import sys
+import os
+
+# Use the current development version of Pyskool
+PYSKOOL_HOME = os.environ.get('PYSKOOL_HOME')
+if not PYSKOOL_HOME:
+    sys.stderr.write('PYSKOOL_HOME is not set; aborting\n')
+    sys.exit(1)
+if not os.path.isdir(PYSKOOL_HOME):
+    sys.stderr.write('PYSKOOL_HOME=%s: directory not found\n' % PYSKOOL_HOME)
+    sys.exit(1)
+sys.path.insert(0, PYSKOOL_HOME)
+
+from pyskool.image import get_images, SKOOL_DAZE, BACK_TO_SKOOL
+
+def parse_args(args):
+    odir = os.path.expanduser(os.path.join('~', '.pyskool'))
+    p_args = []
+    i = 0
+    while i < len(args):
+        arg = args[i]
+        if arg.startswith('-'):
+            print_usage()
+        else:
+            p_args.append(arg)
+        i += 1
+    if len(p_args) != 1:
+        print_usage()
+    return p_args[0]
+
+def print_usage():
+    sys.stderr.write("""Usage: {0} DIRECTORY
+
+  Creates the stock Pyskool image files in a subdirectory named 'images' in
+  DIRECTORY.
+""".format(os.path.basename(sys.argv[0])))
+    sys.exit(1)
+
+###############################################################################
+# Begin
+###############################################################################
+odir = parse_args(sys.argv[1:])
+images_ini = os.path.join(PYSKOOL_HOME, 'images.ini')
+get_images(images_ini, SKOOL_DAZE, 0, odir)
+get_images(images_ini, SKOOL_DAZE, 1, odir)
+get_images(images_ini, SKOOL_DAZE, 2, odir)
+get_images(images_ini, BACK_TO_SKOOL, 0, odir)
+get_images(images_ini, BACK_TO_SKOOL, 1, odir)
