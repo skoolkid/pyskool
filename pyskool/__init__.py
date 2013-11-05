590943c04e50c83576159dbe44cb156a60ec7945
Richard Dymond <rjdymond@gmail.com>
2013-11-05 19:38:55 -0400

    Add a copyright header to pyskool/__init__.py

--- pyskool/__init__.py
+++ pyskool/__init__.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+
+# Copyright 2011-2013 Richard Dymond (rjdymond@gmail.com)
+#
+# This file is part of Pyskool.
+#
+# Pyskool is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Pyskool is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Pyskool. If not, see <http://www.gnu.org/licenses/>.
+
 import os
 
 version = '1.0.2b1'
